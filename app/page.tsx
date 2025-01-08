d98955f6ff2e4f391bb65f33e8a8f565b3c3de4b
kimjy0301 <znzld505@naver.com>
2025-01-08 15:33:44 +0900

    오타수정

--- app/page.tsx
+++ app/page.tsx
@@ -376,7 +376,7 @@ export default function Home() {
                   점수
                 </th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  승/매/무
+                  승/패/무
                 </th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   입상
